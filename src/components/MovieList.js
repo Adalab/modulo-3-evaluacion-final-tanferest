0e361d9bedae62821a7bfe54b2c6d876904086cd
tanferest <tanferest@gmail.com>
2022-05-07 13:00:57 +0200

    add message if there's no search matches

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,16 +1,20 @@
 import MovieItem from "./MovieItem";
 
 function MovieList (props) {
+    if (props.movies.length === 0) {
+        return <p>Ningún título coincide con esta búsqueda. ¡Prueba otra vez!</p>;
+    } else{
     const movieInfo = props.movies.map((item) => {
         return (
         <li key={item.id}>
             <MovieItem item={item} />
         </li>
-        );
+    );
     });
     return <ul>
         {movieInfo}
     </ul>
-}
+    }
+};
 
 export default MovieList;
